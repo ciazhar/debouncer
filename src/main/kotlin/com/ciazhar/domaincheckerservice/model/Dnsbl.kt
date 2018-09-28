4980c4f8764dba81172d92dbc7e5ef958d2b566c
ciazhar <ciazhar.id@gmail.com>
2018-09-28 17:03:37 +0700

    create model dnsbl

---
+++ src/main/kotlin/com/ciazhar/domaincheckerservice/model/Dnsbl.kt
@@ -0,0 +1,40 @@
+package com.ciazhar.domaincheckerservice.model
+
+import io.vertx.core.json.JsonObject
+import java.util.*
+
+class Dnsbl {
+    var id: String? = null
+    var createdAt: Date? = Date()
+    var name: String = ""
+    var url: String = ""
+
+    constructor(){
+        this.id = ""
+        this.createdAt = Date()
+        this.name = ""
+        this.url = ""
+    }
+    constructor(name: String,url :String,id :String, createdAt :String) {
+        this.id = id
+        this.createdAt = Date()
+        this.name = name
+        this.url = url
+    }
+
+    constructor(json: JsonObject) {
+        this.id =json.getString("_id")
+        this.createdAt = Date.from(json.getInstant("createdAt"))
+        this.name = json.getString("name")
+        this.url = json.getString("url")
+    }
+
+    fun toJson(): JsonObject {
+        val json = JsonObject()
+                .put("id",id)
+                .put("name", name)
+                .put("url", url)
+                .put("createdAt", Date(createdAt!!.time).toInstant())
+        return json
+    }
+}
