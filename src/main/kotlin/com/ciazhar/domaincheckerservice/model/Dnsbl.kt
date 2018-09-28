28b227da2cfdcfdfb1da4e8a3c2f5395c771657b
ciazhar <ciazhar.id@gmail.com>
2018-09-28 17:28:06 +0700

    hide createdAt var

--- src/main/kotlin/com/ciazhar/domaincheckerservice/model/Dnsbl.kt
+++ src/main/kotlin/com/ciazhar/domaincheckerservice/model/Dnsbl.kt
@@ -5,26 +5,26 @@ import java.util.*
 
 class Dnsbl {
     var id: String? = null
-    var createdAt: Date? = Date()
+//    var createdAt: Date? = Date()
     var name: String = ""
     var url: String = ""
 
     constructor(){
         this.id = ""
-        this.createdAt = Date()
+//        this.createdAt = Date()
         this.name = ""
         this.url = ""
     }
     constructor(name: String,url :String,id :String, createdAt :String) {
         this.id = id
-        this.createdAt = Date()
+//        this.createdAt = Date()
         this.name = name
         this.url = url
     }
 
     constructor(json: JsonObject) {
         this.id =json.getString("_id")
-        this.createdAt = Date.from(json.getInstant("createdAt"))
+//        this.createdAt = Date.from(json.getInstant("createdAt"))
         this.name = json.getString("name")
         this.url = json.getString("url")
     }
@@ -34,7 +34,7 @@ class Dnsbl {
                 .put("id",id)
                 .put("name", name)
                 .put("url", url)
-                .put("createdAt", Date(createdAt!!.time).toInstant())
+//                .put("createdAt", Date(createdAt!!.time).toInstant())
         return json
     }
 }
